ddf7f19c85779709798ed2ea297b8c731aeaced3
Sendiko <sarangtawon897@gmail.com>
2025-06-04 08:37:56 +0700

    chore: add middleware(forgor)

--- route/route.ts
+++ route/route.ts
@@ -1,13 +1,14 @@
 // @ts-nocheck
 import express, { Router, Request, Response } from "express";
 import OrderController from "../controllers/order_controller";
+import upload from "../middleware/upload";
 
 const router: Router = express.Router();
 
 router.get("/eggs", OrderController.index);
-router.post("/eggs", OrderController.create);
 router.get("/eggs/:id", OrderController.show);
-router.put("/eggs/:id", OrderController.update);
+router.post("/eggs", upload.single("image"), OrderController.create);
+router.put("/eggs/:id", upload.single("image"), OrderController.update);
 router.delete("/eggs/:id", OrderController.delete);
 
 export default router;
